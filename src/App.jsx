4094ad991ade9eb1f6f70f3447de5f3505867dc1
María Noel Silvera <mnsu1710@gmail.com>
2024-01-03 17:56:12 -0300

    Rename ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar/NavBar";
-import ItemListContainer from "./components/itemListContainer/itemListContainer";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
