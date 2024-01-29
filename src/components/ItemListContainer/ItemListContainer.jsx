bb29e87887214d9deda5913e943d3e74474abf5f
María Noel Silvera <mnsu1710@gmail.com>
2024-01-29 20:37:46 -0300

    Fix spinner in ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -17,6 +17,7 @@ const ItemListContainer = () => {
           (product) => product.category === categoryId
         )
         setItems(filteredProducts)
+        setLoading(false)
       } else {
         setItems(products)
         setLoading(false)
