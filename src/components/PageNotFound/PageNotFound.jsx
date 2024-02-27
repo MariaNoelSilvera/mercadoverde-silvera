75bb5f8f93c9094a1d8eddf404183c80cc8f09c1
María Noel Silvera <mnsu1710@gmail.com>
2024-02-26 21:30:44 -0300

    Fix ItemDetail styles

--- src/components/PageNotFound/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './PageNotFound.module.scss'
-
-const PageNotFound = () => {
-  return <div className={styles.PageNotFound}>Page Not Found</div>;
-};
-
-export default PageNotFound;
