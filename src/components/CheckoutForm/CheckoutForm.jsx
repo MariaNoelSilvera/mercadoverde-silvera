7240ab7cc8b35b57a31b9ac6ec712ba27e8927e9
María Noel Silvera <mnsu1710@gmail.com>
2024-02-28 23:19:52 -0300

    Fix Checkout styles

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -25,13 +25,6 @@ const Order = () => {
       const docRef = await addDoc(ordersCollection, { ...formData });
       setDocId(docRef.id);
 
-      setFormData({
-        name: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        address: '',
-      });
       clearCart();
     } catch (error) {
       console.error('Error submitting order:', error);
@@ -42,78 +35,75 @@ const Order = () => {
     <div className={styles.orderContainer}>
       {cart.length > 0 && (
         <div className={styles.formContainer}>
-          {docId ? (
-            <p>Pedido enviado! Nro de orden: {docId}</p>
-          ) : (
-            <>
-              <h3>DATOS DE FACTURACIÓN</h3>
-              <form onSubmit={handleSubmit}>
-                <div className={styles.formGroup}>
-                  <label htmlFor="name">Nombre:</label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    required
-                    value={formData.name}
-                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label htmlFor="lastName">Apellido:</label>
-                  <input
-                    type="text"
-                    id="lastName"
-                    name="lastName"
-                    required
-                    value={formData.lastName}
-                    onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    required
-                    value={formData.email}
-                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label htmlFor="phone">Teléfono:</label>
-                  <input
-                    type="tel"
-                    id="phone"
-                    name="phone"
-                    required
-                    value={formData.phone}
-                    onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label htmlFor="address">Dirección:</label>
-                  <textarea
-                    id="address"
-                    name="address"
-                    rows="4"
-                    required
-                    value={formData.address}
-                    onChange={(e) => setFormData({ ...formData, address: e.target.value })}
-                  />
-                </div>
-                <button className={styles.sendOrder} type="submit">Realizar Pedido</button>
-              </form>
-            </>
-          )}
+          <>
+            <h3>DATOS DE FACTURACIÓN</h3>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formGroup}>
+                <label htmlFor="name">Nombre:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  value={formData.name}
+                  onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="lastName">Apellido:</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  required
+                  value={formData.lastName}
+                  onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="phone">Teléfono:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  required
+                  value={formData.phone}
+                  onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="address">Dirección:</label>
+                <textarea
+                  id="address"
+                  name="address"
+                  rows="4"
+                  required
+                  value={formData.address}
+                  onChange={(e) => setFormData({ ...formData, address: e.target.value })}
+                />
+              </div>
+              <button className={styles.sendOrder} type="submit">Realizar Pedido</button>
+            </form>
+          </>
         </div>
       )}
 
       {docId && cart.length === 0 && (
         <div className={styles.successMessage}>
-          <p>¡Pedido enviado!</p>
-          <p>Nro de orden: {docId}</p>
+          <p>{formData.name}, tu pedido fue enviado.</p>
+          <p>El número de orden es: <span className={styles.docId}>{docId}</span></p>
+           <p>¡Gracias por tu compra!</p>
         </div>
       )}
     </div>
